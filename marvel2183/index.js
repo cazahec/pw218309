fa7ccd1bf2fa71051ce01cf85520feb675e3f7c2
cazahec <cazahec@gmail.com>
2018-11-22 10:07:08 -0700

    api Marvel

--- marvel2183/index.js
+++ marvel2183/index.js
@@ -1,4 +1,24 @@
+const {BrowswerWindow}=require('electron').remote
+const app=require('electron').app
+const path=require('path')
+const url=require('url')
+
 var personaje=""
+let PantallaDetalle;
+
+var btnComics=document.getElementsByClassName('btnComics')
+
+var buscaComics=function(){
+	// alert(this.value)
+	localStorage.setItem("indice",this.value);
+	localStorage.setItem("personaje",personaje);
+	PantallaDetalle=new BrowswerWindow({width:400,height;425});
+	PantallaDetalle.loadURL(url.format({
+		pathname: path.join(__driname,'PantallaDetalle.html'),
+		protocol: 'file',
+		slashes: true
+	}))
+}
 
 var buscaPersonaje=function(){
 	personaje=document.getElementById('txtPersonaje').value;
@@ -18,11 +38,14 @@ var buscaPersonaje=function(){
 				</article>
 				<article class="abajoDerecha">
 					<div class="txtNombre">${datos.data.results[i].name}</div>
-					<button class="btnComics" value="">Comics</button> 
+					<button class="btnComics" value="${i}">Comics</button> 
 				</article>
 				<hr>
 				<br>
 		`
+		} //Termina For
+		for(let i=0;i<btnComics.length;i++){
+			btnComics[i].addEventListener('click',buscaComics);
 		}
 	})
 }
