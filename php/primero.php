be00c91781ed55c9c54cc66398608ed590c53691
cazahec <cazahec@gmail.com>
2018-11-28 09:53:26 -0700

    Primer PHP

---
+++ php/primero.php
@@ -0,0 +1,21 @@
+<?php
+$servidor="localhost";
+$usuario ="root";
+$password="";
+$basedatos="pw09";
+
+$conexion=mysqli_connect($servidor,$usuario,$password,$basedatos);
+$sql="select ncontrol,nombre,apellido, edad from alumnos";
+$resultado=mysqli_query($conexion.$sql);
+var arregloDatos = array();
+if(mysqli_num_rows($resultado)>0){
+	while($registro=mysqli_fetch_array($resultado)){
+		$arregloDatos[] = $registro;
+
+	}
+}
+//print(arregloDatos);//en pantalla
+//si quiereo json
+print json_encode($arregloDatos);
+
+?>
