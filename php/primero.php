dfc9bfd5cf7365174e7e0536baa87e45af738f7e
cazahec <cazahec@gmail.com>
2018-11-28 09:57:42 -0700

    primer php corregido

--- php/primero.php
+++ php/primero.php
@@ -7,7 +7,7 @@ $basedatos="pw09";
 $conexion=mysqli_connect($servidor,$usuario,$password,$basedatos);
 $sql="select ncontrol,nombre,apellido, edad from alumnos";
 $resultado=mysqli_query($conexion.$sql);
-var arregloDatos = array();
+$arregloDatos = array();
 if(mysqli_num_rows($resultado)>0){
 	while($registro=mysqli_fetch_array($resultado)){
 		$arregloDatos[] = $registro;
