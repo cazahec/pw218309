e0e271bb79e7f1b413ec9f74210c69e98809cf28
cazahec <cazahec@gmail.com>
2018-11-29 09:51:42 -0700

    obtener datos Json de mysql

---
+++ php/alumnos/alumnos.php
@@ -0,0 +1,17 @@
+<?php
+	$servidor="localhost";
+	$usuario ="root";
+	$password="";
+	$basedatos="alumnos";
+	$conexion=mysqli_connect($servidor,$usuario,$password,$basedatos);
+	$consulta="select ncontrol,nombre,apellido,edad from alumnositc";
+	$resultado=mysqli_query($conexion,$consulta);
+	$salida = array();
+	if(mysqli_num_rows($resultado) > 0){
+		while($registro=mysqli_fetch_array($resultado)){
+			// $salida[]= $registro;
+			array_push($salida,$registro);
+		}
+	}
+	print json_encode($salida);
+?>
